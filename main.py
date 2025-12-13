34fb75b38f96e68a705a8507d5495468271532c9
LiamSandstrom <liam@Mac.lan>
2025-12-14 00:53:46 +0100

    refactor main loop

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from src.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -13,24 +17,36 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        log_state()
 
-        render(screen, player, dt)
+    while True:
+        if(player_is_quiting() == True): return
+
+        update(updatable, dt)
+        render(screen, drawable)
         print(player.acceleration)
 
         dt = clock.tick(144) / 1000
 
 
 
-def render(screen, player, dt):
+def render(screen, group):
     screen.fill("black")
-    player.update(dt)
-    player.draw(screen)
+    for i in group:
+        i.draw(screen)
     pygame.display.flip()
+
+def update(group, dt):
+    log_state()
+    for i in group:
+        i.update(dt)
+
+def player_is_quiting():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+    return False
+
+
 
 
 if __name__ == "__main__":
